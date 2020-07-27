394aeff329ac7c0cb3b765e9f0c0158a24c562e7
Todd Esposito <todd@toddesposito.com>
2020-07-26 19:23:05 -0500

    Add 'key' targeting in JSON files

--- src/update-target.js
+++ src/update-target.js
@@ -11,7 +11,7 @@
 const fs = require('fs')
 
 module.exports = (target, current, bumped, flags) => {
-  if (!(target.file && target.search)) {
+  if (!(target.file && (target.search || target.key))) {
     throw `Skipping invalid target ${JSON.stringify(target)}.`
   }
   if (!fs.existsSync(target.file)) {
@@ -21,11 +21,33 @@ module.exports = (target, current, bumped, flags) => {
     throw `'${target.file}' is not a regular file. Can't update it!`
   }
   const content = fs.readFileSync(target.file, 'utf8')
-  const from = target.search.replace('{{version}}', current)
-  const to = target.search.replace('{{version}}', bumped)
-  const updated = content.replace(from, to)
-  if (updated === content) {
-    throw `Couldn't find the search string in ${target.file}!`
+  var updated
+  if (target.search) {
+    const from = target.search.replace('{{version}}', current)
+    const to = target.search.replace('{{version}}', bumped)
+    updated = content.replace(from, to)
+    if (updated === content) {
+      throw `Couldn't find the search string in ${target.file}!`
+    }
+  } else {
+    var jsoncontent
+    try {
+      jsoncontent = JSON.parse(content)
+    } catch (error) {
+      throw `${target.file} does not appear to be a valid JSON file.`
+    }
+    var node = jsoncontent
+    const keys = target.key.split('.')
+    keys.slice(0, -1).forEach(key => {
+      if (node[key] === undefined) {
+        throw `${target.file} does not have the specified key: ${target.key}.`
+      }
+      node = node[key]
+    })
+    if (node[keys.slice(-1)] === current) {
+      node[keys.slice(-1)] = bumped
+      updated = JSON.stringify(jsoncontent)
+    }
   }
   if (!flags['dry-run']) {
     fs.writeFileSync(target.file, updated)
